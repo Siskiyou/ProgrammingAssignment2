7c58a11d5410a1ae731d77cdccb629ccda88fc86
Gene Schultz <geneschultz74@gmail.com>
2015-11-22 00:05:34 -0600

    AssignmentCodeAdded
    Added the code for Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+##When a user calls the makeCacheMatrix function, they can enter a square matrix (equal column and row lengths)
+##The inverse of that matrix will then be assigned to an output variable and stored in cache
+## The cacheSolve function allows a user to assign a matrix variable to it and will then return the inverse of that matrix
+## If no matrix variable is assigned by a user, the inverse matrix stored in the cache will be returned
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix receives a previously defined matrix and stores the inverse matrix in the cache
+makeCacheMatrix <- function (inputMatrix = matrix()) {
+  ##Define setter and initialize input and output values
+  outputMatrix <- NULL
+  set <- function(cacheMatrix) {
+    inputMatrix <<- cacheMatrix 
+    outputMatrix <<- NULL
+  }
+  ##Define getter for input and getter/setter for inverse matrices
+  get <- function() inputMatrix
+  setinverse <- function(solve) outputMatrix <<- solve
+  getinverse <- function() outputMatrix
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrixEntry, ...)
+{
+  ##If no new matrix is assigned to cacheSolve, then the cached inverse matrix will be assigned
+  cacheMatrix <- matrixEntry$getinverse()
+  if(!is.null(cacheMatrix))
+  {
+    message("Getting Cached Matrix")
+    return(cacheMatrix)
+  }
+  data <- matrixEntry$get()
+  cacheMatrix <- solve(data, ...)
+  matrixEntry$setinverse(cacheMatrix)
+  cacheMatrix
 }
